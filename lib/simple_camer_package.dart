383d47c6d41bb4c470e4800545c4778944f22c3c
albez <a.bezdenezhnykh.o@gmail.com>
2025-09-21 10:56:21 +0500

    fix bug

--- lib/simple_camer_package.dart
+++ lib/simple_camer_package.dart
@@ -1,4 +1,16 @@
 library;
 
 // Реэкспортируем XFile из image_picker
-export "package:cross_file/src/types/interface.dart" show XFile;
+
+export 'package:camera_platform_interface/camera_platform_interface.dart'
+    show
+        CameraDescription,
+        CameraException,
+        CameraLensDirection,
+        CameraLensType,
+        ExposureMode,
+        FlashMode,
+        FocusMode,
+        ImageFormatGroup,
+        ResolutionPreset,
+        XFile;
